d9a8e9bcba83423a634fcb23875989dcd21ef51b
connerkup <connerkup@gmail.com>
2023-01-26 23:56:09 -0800

    got cross-references working

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
-const express = require("express")
-const mongo = require("mongodb").MongoClient
+const express = require("express");
+const mongo = require("mongodb").MongoClient;
+const crypto = require('crypto');
+const { ObjectId } = require('mongodb');
 
 const uri = process.env.MONGO_DB_URI
 
@@ -22,31 +24,60 @@ mongo.connect(
     db = client.db("launchfizz")
     ideas = db.collection("ideas")
     users = db.collection("users")
+    seeds = db.collection("seeds")
     console.log("Connected to MongoDB succesfully")
   }
 )
 
 app.use(express.json())
 
+function generateSeed() {
+  return crypto.randomBytes(16).toString('hex');
+}
 
+// Create an Idea
 app.post("/ideas", (req, res) => {
-  ideas.insertOne(
-    {
-      seed: req.body.seed,
-      idea_name: req.body.idea_name,
-      description: req.body.description
-    },
+  const userId = req.body.userID;
+  const userFilter = { _id: new ObjectId(userId) }
+  const ideaToInsert = {
+    userID: userId,
+    seed: generateSeed(),
+    idea_name: req.body.idea_name,
+    description: req.body.description
+  }
+
+  ideas.insertOne(ideaToInsert,
     (err, result) => {
       if (err) {
         console.error(err)
         res.status(500).json({ err: err })
         return
+      } else {
+        console.log(ideaToInsert._id)
+        const ideaID = ideaToInsert._id;
+        users.updateOne(
+          userFilter,
+          {
+            $addToSet:
+            {
+              favorites: ideaID
+            }
+          },
+          (err, result) => {
+            if (err) {
+              console.log("error executing user update")
+              console.error(err)
+              res.status(500).json({ err: err })
+              return;
+            }
+            res.status(200).json({ ok: true })
+          }
+        )
       }
-      console.log(result)
-      res.status(200).json({ ok: true })
-    })
+    });
 })
 
+// Return all Ideas
 app.get("/ideas", (req, res) => {
   ideas.find().toArray((err, items) => {
     if (err) {
@@ -58,11 +89,13 @@ app.get("/ideas", (req, res) => {
   })
 });
 
+// Create a new user
 app.post("/users", (req, res) => {
   users.insertOne(
     {
       name: req.body.name,
-      username: req.body.username
+      username: req.body.username,
+      favorites: []
     },
     (err, result) => {
       if (err) {
@@ -74,6 +107,26 @@ app.post("/users", (req, res) => {
     }
   )
 })
+
+// Favorite an idea
+app.put("/users/:userId/favorites/:ideaId", (req, res) => {
+  let userId = new ObjectId(req.params.userId);
+  let ideaId = req.params.ideaId;
+  console.log(userId)
+  console.log(ideaId)
+  users.updateOne(
+    { _id: userId },
+    { $addToSet: { favorites: ideaId } },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ err: err });
+        return;
+      }
+      res.status(200).json({ ok: true });
+    }
+  );
+});
 
 app.get("/users", (req, res) => {
   /* */
